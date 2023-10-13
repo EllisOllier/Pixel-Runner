2edb5c0ee147e8374155ac88ef1fcbb9ef0096b4
EllisOllier <ellisollier1@gmail.com>
2023-10-13 22:03:46 +0100

    Added audio and Removed redundant code

--- main.py
+++ main.py
@@ -19,10 +19,14 @@ class Player(pygame.sprite.Sprite):
         self.rect = self.image.get_rect(midbottom = (80,300))
         self.gravity = 0
 
+        self.jump_sound = pygame.mixer.Sound('audio/jump.mp3')
+        self.jump_sound.set_volume(0.25)
+
     def player_input(self):
         keys = pygame.key.get_pressed()
         if keys[pygame.K_SPACE] and self.rect.bottom >= 300:
             self.gravity = -20
+            self.jump_sound.play()
 
     def apply_gravity(self):
         self.gravity += 1
@@ -87,47 +91,12 @@ def display_score():
 
     return current_time
 
-def obstacle_movement(obstacle_list):
-    if obstacle_list:
-        for obstacle_rect in obstacle_list:
-            obstacle_rect.x -= obstacle_speed
-
-            if obstacle_rect.bottom == 300:
-                screen.blit(snail_surf, obstacle_rect)
-            else:
-                screen.blit(fly_surf, obstacle_rect)
-
-        obstacle_list = [obstacle for obstacle in obstacle_list if obstacle.x > -100]
-        
-        return obstacle_list
-    else:
-        return []
-
-def collisions(player, obstacles):
-    if obstacles:
-        for obstacle_rect in obstacles:
-            if player.colliderect(obstacle_rect):
-                return False
-    return True
-
 def collision_sprite():
     if pygame.sprite.spritecollide(player.sprite, obstacle_group, False):
         obstacle_group.empty()
         return False
     else:
         return True
-
-
-def player_animation():
-    global player_surf, player_index
-
-    if player_rect.bottom < 300:
-        player_surf = player_jump
-    else:
-        player_index += 0.1
-        if player_index >= len(player_walk):     
-            player_index = 0
-        player_surf = player_walk[int(player_index)]
 
 # Game init
 pygame.init()
@@ -137,7 +106,6 @@ pygame.display.set_caption('Pixel Jumper')
 # Class vars
 player = pygame.sprite.GroupSingle()
 player.add(Player())
-
 obstacle_group = pygame.sprite.Group()
 
 # Game vars
@@ -147,43 +115,13 @@ game_active = False
 start_time = 0
 score_div_modifier = 100
 game_floor = 300
-x_spawn_pos = randint(900, 1100)
 score = 0
+background_music = pygame.mixer.Sound('audio/music.wav')
+background_music.set_volume(0.25)
 
 # Env vars
 sky_surf = pygame.image.load('graphics/Sky.png').convert()
 ground_surf = pygame.image.load('graphics/Ground.png').convert()
-
-# Obstacles
-
-obstacle_rect_list = []
-obstacle_speed = 5
-
-    # Snail vars
-snail_frame_1 = pygame.image.load('graphics/snail/snail1.png').convert_alpha()
-snail_frame_2 = pygame.image.load('graphics/snail/snail2.png').convert_alpha()
-snail_frames = [snail_frame_1, snail_frame_2]
-snail_frame_index = 0
-snail_surf = snail_frames[snail_frame_index]
-
-    # Fly vars
-fly_frame_1 = pygame.image.load('graphics/fly/Fly1.png').convert_alpha()
-fly_frame_2 = pygame.image.load('graphics/fly/Fly2.png').convert_alpha()
-fly_frames = [fly_frame_1, fly_frame_2]
-fly_frame_index = 0
-fly_surf = fly_frames[fly_frame_index]
-
-# Player vars
-player_jump = pygame.image.load('graphics/Player/jump.png').convert_alpha()
-player_walk_1 = pygame.image.load('graphics/Player/player_walk_1.png').convert_alpha()
-player_walk_2 = pygame.image.load('graphics/Player/player_walk_2.png').convert_alpha()
-player_walk = [player_walk_1, player_walk_2]
-player_index = 0
-
-player_surf = player_walk[player_index]
-player_rect = player_walk_1.get_rect(midbottom = (80,game_floor))
-player_gravity = 0
-player_jump_height = -20
 
 # Main menu
 player_stand = pygame.image.load('graphics/Player/player_stand.png').convert_alpha()
@@ -200,49 +138,21 @@ restart_game_rect = restart_game_surf.get_rect(midbottom = (400,350))
 obstacle_timer = pygame.USEREVENT + 1
 pygame.time.set_timer(obstacle_timer, 1500)
 
-snail_animation_timer = pygame.USEREVENT + 2
-pygame.time.set_timer(snail_animation_timer, 500)
-
-fly_animation_timer = pygame.USEREVENT + 3
-pygame.time.set_timer(fly_animation_timer, 200)
-
 # Game loop
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
-
+        
         if game_active:
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                if player_rect.collidepoint((event.pos)):
-                    if player_rect.bottom == game_floor:
-                        player_gravity = player_jump_height   
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_SPACE:
-                    if player_rect.bottom == game_floor:
-                        player_gravity = player_jump_height  
+            if event.type == obstacle_timer:
+                obstacle_group.add(Obstacle(choice(['fly', 'snail', 'snail', 'snail'])))
         else:
             if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
                 start_time = int(pygame.time.get_ticks() / score_div_modifier)
                 game_active = True
-        if game_active:
-            if event.type == obstacle_timer:
-                obstacle_group.add(Obstacle(choice(['fly', 'snail', 'snail', 'snail'])))
-            
-            if event.type == snail_animation_timer:
-                if snail_frame_index == 0:
-                    snail_frame_index = 1
-                else:
-                    snail_frame_index = 0
-                snail_surf = snail_frames[snail_frame_index]
-            
-            if event.type == fly_animation_timer:
-                if fly_frame_index == 0:
-                    fly_frame_index = 1
-                else:
-                    fly_frame_index = 0
-                fly_surf = fly_frames[fly_frame_index]
+                background_music.play(loops = -1)
 
     if game_active:
         screen.blit(sky_surf, (0,0))
@@ -271,9 +181,8 @@ while True:
         if score > 0:
             screen.blit(score_surf, score_rect)
 
-        obstacle_rect_list.clear()
-        player_rect.midbottom = (80, game_floor)
         player_gravity = 0
+        background_music.stop()
             
     pygame.display.update()
     clock.tick(60)
